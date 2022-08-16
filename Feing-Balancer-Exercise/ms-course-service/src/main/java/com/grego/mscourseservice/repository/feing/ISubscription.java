4b7019d4e83e06bc0c7d041ca594bfc28b5147f0
Grego Garcia <82552333+Dgarciadani@users.noreply.github.com>
2022-08-15 22:21:33 -0300

    [ADD] Custom Random Load Balancer

--- Feing-Balancer-Exercise/ms-course-service/src/main/java/com/grego/mscourseservice/repository/feing/ISubscription.java
+++ Feing-Balancer-Exercise/ms-course-service/src/main/java/com/grego/mscourseservice/repository/feing/ISubscription.java
@@ -1,12 +1,15 @@
 package com.grego.mscourseservice.repository.feing;
 
+import com.grego.mscourseservice.configuration.CustomLoadBalancerRandom;
 import com.grego.mscourseservice.domain.Subscription;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "subscription-service")
+@LoadBalancerClient(name = "subscription-service", configuration = CustomLoadBalancerRandom.class)
 public interface ISubscription {
 
     @GetMapping("/subscription/find")
